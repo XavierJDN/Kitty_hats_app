55f50e8a90a2a71607265abe39154afca749f507
Xavier Jouslin <jouslin@nowray.net>
2023-01-25 14:53:31 -0500

    fix: add input file to compile the project

---
+++ server/app/app.module.ts
@@ -0,0 +1,21 @@
+import { Logger, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (config: ConfigService) => ({
+                uri: config.get<string>('DATABASE_CONNECTION_STRING'), // Loaded from .env
+            }),
+        }),
+        MongooseModule.forFeature([]), // create the mogoDB database (shema = { name: string, schema:  <service>})
+    ],
+    controllers: [], // all controllers are imported here
+    providers: [Logger],
+})
+export class AppModule {}
