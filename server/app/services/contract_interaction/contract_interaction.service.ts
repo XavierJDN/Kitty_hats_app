0098de21a4af27457c7c083d4f743810016f83a0
Xavier Jouslin <jouslin@nowray.net>
2023-01-26 06:32:51 -0500

    fix: remove unessacery variables

--- server/app/services/contract_interaction/contract_interaction.service.ts
+++ server/app/services/contract_interaction/contract_interaction.service.ts
@@ -9,24 +9,19 @@ export class ContractInteractionService {
   // ...
   public async getContractInstance(
     contractName: string,
-    contractAddress: string,
     web3: Web3,
   ): Promise<Contract> {
-    const contract = new web3.eth.Contract(
+    return new web3.eth.Contract(
       await this.getContractAbi(contractName),
-      contractAddress,
+      await this.getContractAddress(contractName),
     );
-    return contract;
   }
 
-  // create a method to get the contract ABI from the contract name
     private async getContractAbi(contractName: string): Promise<AbiItem> {
-        // get the contract ABI from the contract name
         return JSON.parse(((await FsManager.readFile(`@app/assets/${contractName}/abi.json`))).toString());
     }
 
-    // get the contract addresss from the contract name
-    public getContractAddress(contractName: string): string {
-        return JSON.parse(((FsManager.readFile(`@app/assets/${contractName}/index.json`))).toString())['address'];
+    private async getContractAddress(contractName: string): Promise<string> {
+        return JSON.parse(((await FsManager.readFile(`@app/assets/${contractName}/index.json`))).toString())['address'];
     }
 }
