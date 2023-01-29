4b53e030140d927c85e03e27513b675e10fe875a
Xavier Jouslin <jouslin@nowray.net>
2023-01-29 14:19:05 -0500

    fix: add market contract for kitty token

--- server/app/services/kitty_token_market_contract_manager/kitty_token_market_contract_manager.service.ts
+++ server/app/services/kitty_token_market_contract_manager/kitty_token_market_contract_manager.service.ts
@@ -1,13 +1,14 @@
 import { ContractInteractionService } from "@app/services/contract_interaction/contract_interaction.service";
+import { Injectable } from "@nestjs/common";
 import { Contract } from 'web3-eth-contract';
 
-
-export class KittyTokenContractManagerService {
+@Injectable()
+export class KittyTokenMarketContractManagerService {
   private contract: Contract;
 
     constructor(private contractInteractionService: ContractInteractionService) {}
 
   async setContract() {
-    return this.contract = await this.contractInteractionService.getContractInstance("kitty_token");
+    return this.contract = await this.contractInteractionService.getContractInstance("kitty_token_market");
   }
 }
