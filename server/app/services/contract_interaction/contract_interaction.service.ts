5100751d04675684d0b988867380ce75c6045174
Xavier Jouslin <jouslin@nowray.net>
2023-01-29 13:45:09 -0500

    feat: add kitty token contract manager

--- server/app/services/contract_interaction/contract_interaction.service.ts
+++ server/app/services/contract_interaction/contract_interaction.service.ts
@@ -11,25 +11,27 @@ export class ContractInteractionService {
   constructor() {
     if(!this.web3){
       this.web3 = new Web3('https://mainnet.infura.io/v3/971ed22e4e2e4276b37166adebf4b1ac');
+      this.web3.eth.transactionConfirmationBlocks = 30;
     }
     if(!Contract.currentProvider){
       Contract.setProvider(this.web3.currentProvider);
     }
   }
+
   public async getContractInstance(
     contractName: string,
   ): Promise<typeof Contract> {
-    return new Contract(
+    return new this.web3.eth.Contract(
       await this.getContractAbi(contractName),
       await this.getContractAddress(contractName),
     );
   }
 
-    private async getContractAbi(contractName: string): Promise<AbiItem> {
-        return JSON.parse(((await FsManager.readFile(`./assets/contracts/${contractName}/abi.json`))).toString());
-    }
+  private async getContractAbi(contractName: string): Promise<AbiItem> {
+    return JSON.parse(((await FsManager.readFile(`./assets/contracts/${contractName}/abi.json`))).toString());
+  }
 
-    private async getContractAddress(contractName: string): Promise<string> {
-        return JSON.parse(((await FsManager.readFile(`./assets/contracts/${contractName}/index.json`))).toString())['address'];
-    }
+  private async getContractAddress(contractName: string): Promise<string> {
+    return JSON.parse(((await FsManager.readFile(`./assets/contracts/${contractName}/index.json`))).toString())['address'];
+  }
 }
--- server/app/services/kitty_contract_manager/kitty_contract_manager.service.ts
+++ server/app/services/kitty_contract_manager/kitty_contract_manager.service.ts
@@ -8,6 +8,45 @@ export class KittyContractManagerService {
     constructor(private contractInteractionService: ContractInteractionService) {}
 
   async setContract() {
-    this.contract = await this.contractInteractionService.getContractInstance("crypto_kitty");
+    return this.contract = await this.contractInteractionService.getContractInstance("crypto_kitty");
+  }
+
+  async tokensOf(address: string): Promise<{kitty: any}[]> {
+    return this.getAllTokens().then((tokens: {kitty: any, owner: string}[]) => {
+      return tokens.filter((token: { kitty: any, owner: string}) => token.owner === address).map((token: {kitty: any, owner: string}) => token.kitty);
+    });
+  }
+
+  async getAllTokens(){
+    const tokens = [];
+    const supply = await this.getAllSupply();
+    for (let id = 1; id < supply; id++) {
+      let kitty: any;
+      let owner: string;
+      try{
+        kitty = await this.contract.methods.getKitty(id).call();
+        owner = await this.contract.methods.ownerOf(id).call();
+      }catch(e){
+        return tokens;
+      }
+      console.log(kitty);
+      tokens.push({ ...kitty, owner});
+      }
+    return tokens;
+  }
+
+  async getAllSupply(): Promise<any> {
+    try{
+      return await this.contract.methods.totalSupply().call();
+    }catch(e){
+      return null;
+    }
+  }
+  checkBalance(address: string): Promise<any> {
+    try{
+      return this.contract.methods.balanceOf(address).call();
+    }catch(e){
+      return null;
+    }
   }
 }
