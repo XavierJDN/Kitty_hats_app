e23572674df432c8e5ec201051406965df26e2be
Xavier Jouslin <jouslin@nowray.net>
2023-02-01 09:07:23 -0500

    fix: remove documentation for fs

--- server/app/services/fs_manager/fs_manager.service.ts
+++ server/app/services/fs_manager/fs_manager.service.ts
@@ -4,20 +4,10 @@ import { promises, constants } from 'fs';
 export class FsManager{
     constructor() {}
 
-    /**
-     * Vérifie si un fichier existe avec fs
-     * @param file: le fichier à vérifier
-     * @returns rien si le fichier existe. Sinon, lancer une erreur
-     */
     async checkFile(file: string): Promise<void> {
       return await promises.access(file, constants.R_OK);
     }
   
-    /**
-     * Lit et retourne le contenu d'un fichier
-     * @param {string} path : le chemin qui correspond au fichier JSON
-     * @returns {Promise<Buffer>} le cotenu du fichier sous la forme de Buffer
-     */
     static async readFile(path: string): Promise<Buffer> {
       return await promises.readFile(path);
     }
