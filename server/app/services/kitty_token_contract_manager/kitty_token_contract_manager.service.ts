f22bbcf4885265e0dd228955ac6f223af53d07fa
Xavier Jouslin <jouslin@nowray.net>
2023-02-01 09:09:04 -0500

    feat: get image of a token nft

--- server/app/services/kitty_token_contract_manager/kitty_token_contract_manager.service.ts
+++ server/app/services/kitty_token_contract_manager/kitty_token_contract_manager.service.ts
@@ -1,11 +1,10 @@
 import { ContractInteractionService } from "@app/services/contract_interaction/contract_interaction.service";
 import { Injectable } from "@nestjs/common";
-import { Contract } from 'web3-eth-contract';
-import { FsManager } from "../fs_manager/fs_manager.service";
-import { access, constants } from 'fs';
+import { FsManager } from "@app/services/fs_manager/fs_manager.service";
+import { access, constants } from "fs";
+import { KittyTokenMarketContractManagerService } from "@app/services/kitty_token_market_contract_manager/kitty_token_market_contract_manager.service";
 
 @Injectable()
-
 export class KittyTokenContractManagerService {
   constructor(
     private contractInteractionService: ContractInteractionService,
@@ -26,12 +25,22 @@ export class KittyTokenContractManagerService {
     return JSON.parse((await FsManager.readFile(`../contracts/build/contracts/${this.token.contract}.json`)).toString()).abi;
   }
   async getImage(address: string, isAsset: boolean = false) {
-    if(!this.token) await this.info(address);
-    return access(`../kitty-hats-manifest/build/${isAsset ? 'asset': 'preview' }/${this.token.assetUrl}.png`, constants.F_OK, async (err) =>
-      { return {
-        img: await FsManager.readFile(`../kitty-hats-manifest/build/${isAsset ? 'asset': 'preview' }/${this.token.assetUrl}.${err ? 'svg': 'png'}`),
-        isPng: !err
+    const token = await this.info(address); 
+    return access(
+      `../kitty-hats-manifest/build/${isAsset ? "asset" : "preview"}/${
+        token.assetUrl
+      }.png`,
+      constants.F_OK,
+      async (err) => {
+        return {
+          address: token.tokenAddress,
+          img: await FsManager.base64Encode(
+            `../kitty-hats-manifest/build/${isAsset ? "asset" : "preview"}/${
+              token.assetUrl
+            }.${err ? "svg" : "png"}`
+          ),
+        };
       }
-    });
+    );
   }
 }
