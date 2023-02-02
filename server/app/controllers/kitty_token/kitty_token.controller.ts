a96a5c207c6bb03e29d39725297c07bd1e0c786f
Xavier Jouslin <jouslin@nowray.net>
2023-02-02 07:18:37 -0500

    fix: bug on the token controller

--- server/app/controllers/kitty_token/kitty_token.controller.ts
+++ server/app/controllers/kitty_token/kitty_token.controller.ts
@@ -4,22 +4,28 @@ import { KittyTokenMarketContractManagerService } from "@app/services/kitty_toke
 import { Controller, Get, Param } from "@nestjs/common";
 
 
-@Controller("kitty_token")
+@Controller("tokens")
 export class KittyTokenController {
   constructor(
     private kittyTokenContractManagerService: KittyTokenContractManagerService,
     private kittyTokenMarketContractManagerService: KittyTokenMarketContractManagerService
   ) {}
+  
   @Get("/")
   async tokens() {
-    return this.kittyTokenMarketContractManagerService.tokens.map(
-      async (token) => {
-        return await this.kittyTokenContractManagerService.getInfo(token.tokenAddress);
-      }
-    );
+    await this.kittyTokenMarketContractManagerService.infos();
+    return await Promise.all(this.kittyTokenMarketContractManagerService.tokens.map(async (token) =>
+      await this.kittyTokenContractManagerService.getInfo(token.tokenAddress)
+    ));
   }
-  @Get('contract/:address')
-    async contract(@Param('address') address) {
+
+  @Get('/:address')
+  async token(@Param('address') address: string) {
+    return await this.kittyTokenContractManagerService.getInfo(address);
+  }
+
+  @Get('/contract/:address')
+    async contract(@Param('address') address: string) {
         return await this.kittyTokenContractManagerService.setContract(address);
   }
 }
