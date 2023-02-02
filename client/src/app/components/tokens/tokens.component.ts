cabce2764924463752cfe015f14bff317259713e
Xavier Jouslin <jouslin@nowray.net>
2023-02-02 09:14:25 -0500

    feat: add list of token but cors bug

--- client/src/app/components/tokens/tokens.component.ts
+++ client/src/app/components/tokens/tokens.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommunicationService } from '@app/services/communication/communication.service';
+import { Token } from '@common/token';
 @Component({
   selector: 'app-tokens',
   templateUrl: './tokens.component.html',
   styleUrls: ['./tokens.component.scss']
 })
 export class TokensComponent implements OnInit {
-
-  constructor() { }
+  tokens: Token[] = [];
+  constructor( private communication: CommunicationService) { }
 
   ngOnInit(): void {
+    this.communication.getTokens().subscribe((tokens: Token[]) => (this.tokens = tokens));
   }
 
 }
