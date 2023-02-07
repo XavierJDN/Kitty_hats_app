7144002a754bd04e72acab23acfab33a1dc80b6b
Xavier Jouslin <jouslin@nowray.net>
2023-02-07 08:35:51 -0500

    feat: get tokens info

--- client/src/app/components/tokens/tokens.component.ts
+++ client/src/app/components/tokens/tokens.component.ts
@@ -1,3 +1,4 @@
+import { HttpResponse, HttpStatusCode } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommunicationService } from '@app/services/communication/communication.service';
 import { Token } from '@common/token';
@@ -7,11 +8,14 @@ import { Token } from '@common/token';
   styleUrls: ['./tokens.component.scss']
 })
 export class TokensComponent implements OnInit {
-  tokens: Token[] = [];
+  tokens: Token[] | null = [];
   constructor( private communication: CommunicationService) { }
 
   ngOnInit(): void {
-    this.communication.getTokens().subscribe((tokens: Token[]) => (this.tokens = tokens));
+    this.communication.getTokens().subscribe((response: HttpResponse<Token[]>) => {
+      this.tokens = response.status === HttpStatusCode.Ok ? response.body : [];
+      console.log(this.tokens!.find(token => token.address === '0x54616a975dec9256f8fe26a99b2fcd6affbf6eaf'))
+    });
   }
 
 }
