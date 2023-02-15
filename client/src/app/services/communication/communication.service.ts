5273bba061a227f73cce7dea14137e4701eb9809
Xavier Jouslin <jouslin@nowray.net>
2023-02-15 10:29:05 -0500

    feat: change communication to allow post method

--- client/src/app/services/communication/communication.service.ts
+++ client/src/app/services/communication/communication.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { environment } from '@app/../environments/environment';
 import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommunicationService {
   private readonly baseUrl = environment.serverUrl;
   constructor(private readonly http: HttpClient) {}
 
-  getTokens(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/tokens`, {observe: 'response'});
+  getTokens(tokenId: string | undefined, isNext: boolean = true): Observable<any> {
+    return !tokenId
+      ? this.initializeTokensPage()
+      : this.http.post(
+          `${this.baseUrl}/tokens/`,
+          { id: tokenId, isNext },
+          { observe: 'response' }
+        );
   }
 
-  getToken(id: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}/tokens/${id}`, {observe: 'response'});
+  initializeTokensPage(limit: number = 30, state: number = 0): Observable<any> {
+    return this.http
+      .post(
+        `${this.baseUrl}/tokens/`,
+        { limit, state },
+        { observe: 'response' }
+      )
   }
 }
