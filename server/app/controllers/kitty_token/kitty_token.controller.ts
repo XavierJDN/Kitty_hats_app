14d79383dd2e329e47e0d67cfc98dc6c7c572474
Xavier Jouslin <jouslin@nowray.net>
2023-02-15 10:29:53 -0500

    feat: change controller to allow token with page manager

--- server/app/controllers/kitty_token/kitty_token.controller.ts
+++ server/app/controllers/kitty_token/kitty_token.controller.ts
@@ -1,31 +1,66 @@
-import { ContractInteractionService } from "@app/services/contract_interaction/contract_interaction.service";
 import { KittyTokenContractManagerService } from "@app/services/kitty_token_contract_manager/kitty_token_contract_manager.service";
 import { KittyTokenMarketContractManagerService } from "@app/services/kitty_token_market_contract_manager/kitty_token_market_contract_manager.service";
-import { Controller, Get, Param } from "@nestjs/common";
-
+import { PageManagerService } from "@app/services/page_manager/page_manager.service";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Logger,
+  Param,
+  Post,
+  Query,
+  Res,
+} from "@nestjs/common";
+import { Response } from "express";
 
 @Controller("tokens")
 export class KittyTokenController {
   constructor(
     private kittyTokenContractManagerService: KittyTokenContractManagerService,
-    private kittyTokenMarketContractManagerService: KittyTokenMarketContractManagerService
+    private kittyTokenMarketContractManagerService: KittyTokenMarketContractManagerService,
+    private pageManager: PageManagerService
   ) {}
-  
-  @Get("/")
-  async tokens() {
+
+  @Post("/")
+  async tokens(@Res() response: Response, @Body() body: any) {
     await this.kittyTokenMarketContractManagerService.infos();
-    return await Promise.all(this.kittyTokenMarketContractManagerService.tokens.map(async (token) =>
-      await this.kittyTokenContractManagerService.getInfo(token.tokenAddress)
-    ));
+    if (body.id === undefined) {
+      if (body.limit === undefined || body.state === undefined)
+        return response.status(HttpStatus.BAD_REQUEST).send();
+      return response
+        .status(HttpStatus.OK)
+        .send(
+          this.pageManager.insert(
+            body.limit,
+            body.state,
+            await Promise.all(
+              this.kittyTokenMarketContractManagerService.tokens.map(
+                async (token) =>
+                  await this.kittyTokenContractManagerService.getInfo(
+                    token.tokenAddress
+                  )
+              )
+            )
+          )
+        );
+    }
+    const page = this.pageManager.next(
+      body.id,
+      body.isNext !== undefined ? body.isNext : true
+    );
+    return response
+      .status(!page ? HttpStatus.BAD_REQUEST : HttpStatus.OK)
+      .send(!page ? null : page);
   }
 
-  @Get('/:address')
-  async token(@Param('address') address: string) {
+  @Get("/:address")
+  async token(@Param("address") address: string) {
     return await this.kittyTokenContractManagerService.getInfo(address);
   }
 
-  @Get('/contract/:address')
-    async contract(@Param('address') address: string) {
-        return await this.kittyTokenContractManagerService.setContract(address);
+  @Get("/contract/:address")
+  async contract(@Param("address") address: string) {
+    return await this.kittyTokenContractManagerService.setContract(address);
   }
 }
