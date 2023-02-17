dc6da0ebecfedc5c30be31955585fa9e26fe7fc7
Xavier Jouslin <jouslin@nowray.net>
2023-02-17 08:42:06 -0500

    test: contract kitty manager

--- server/app/services/kitty_contract_manager/kitty_contract_manager.service.spec.ts
+++ server/app/services/kitty_contract_manager/kitty_contract_manager.service.spec.ts
@@ -26,4 +26,43 @@ describe("KittyCOntractManagerService", () => {
         expect(service['contractInteractionService'].getContractInstance).toHaveBeenCalledWith("crypto_kitty");
         expect(service['contract']).toBeDefined();
     });
-});
+
+    it('tokensof should get all kitty tokens of a address', () => {
+        const expectedTokens = 'kitty';
+        const expectedOwner = '0x123';
+        jest.spyOn(service, 'getAllTokens').mockReturnValue(Promise.resolve([{ kitty: expectedTokens, owner: expectedOwner}]));
+        service.tokensOf(expectedOwner).then((tokens) => {
+            expect(tokens).toEqual([expectedTokens]);
+        }
+        );
+    });
+
+    it('should get all supply', () => {
+        const expectedSupply = 100;
+        service['contract'] = {methods: {totalSupply: () => ({call: async () => new Promise(() => expectedSupply)})}} as unknown as Contract;
+        service.getAllSupply().then((supply) => {
+            expect(supply).toEqual(expectedSupply);
+        });
+    });
+
+    it('should check the balance', () => {
+        const expectedBalance = 100;
+        const expectedAddress = '0x123';
+        service['contract'] = { methods: {balanceOf: () => ({ call: async () => new Promise(() => expectedBalance) }) }} as unknown as Contract;
+        service.checkBalance(expectedAddress).then((balance) => {
+            expect(balance).toEqual(expectedBalance);
+        }
+        );
+    });
+
+    it('should get all tokens', () => {
+        const expectedTokens = [{kitty: 'kitty', owner: '0x123'}];
+        const expectedSupply = 1;
+        jest.spyOn(service, 'getAllSupply').mockReturnValue(Promise.resolve(expectedSupply));
+        service['contract'] = { methods: {getKitty: () => ({call: async () => new Promise(() => expectedTokens[0].kitty)}), ownerOf: () => ({ call: async () => new Promise(() => expectedTokens[0].owner)})}} as unknown as Contract;
+        service.getAllTokens().then((tokens) => {
+            expect(tokens).toEqual(expectedTokens);
+            }
+        );
+    });
+  });
