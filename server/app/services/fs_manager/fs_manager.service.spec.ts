ab1866be9fddcd12f188a39a2567b6eddcf0fccd
Xavier Jouslin <jouslin@nowray.net>
2023-02-17 08:41:44 -0500

    test: fs manager

--- server/app/services/fs_manager/fs_manager.service.spec.ts
+++ server/app/services/fs_manager/fs_manager.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test } from '@nestjs/testing';
 import { FsManager } from '@app/services/fs_manager/fs_manager.service';
-import * as fs from 'fs';
+import { promises, constants, Dirent } from 'fs';
+
 import * as path from 'path';
 
 describe('FsManager', () => {
@@ -20,17 +21,42 @@ describe('FsManager', () => {
 
   it('checkFile should check if a file exists', async () => {
     const filePath = path.join(__dirname, 'file.txt');
-    jest.spyOn(fs.promises, 'access').mockImplementation(() => Promise.resolve());
+    jest.spyOn(promises, 'access').mockImplementation(() => Promise.resolve());
     await service.checkFile(filePath);
-    expect(fs.promises.access).toHaveBeenCalledWith(filePath, fs.constants.R_OK);
+    expect(promises.access).toHaveBeenCalledWith(filePath, constants.R_OK);
+  });
+
+  it('throws an error if file does not exist', async () => {
+    await expect(new FsManager().checkFile('non-existent-file.txt')).rejects.toThrow();
   });
 
   it('readFile should read the contents of a file', async () => {
     const filePath = path.join(__dirname, 'file.txt');
     const fileContents = 'Hello World';
-    jest.spyOn(fs.promises, 'readFile').mockImplementation(() => Promise.resolve(fileContents));
-    const result = await FsManager.readFile(filePath);
-    expect(fs.promises.readFile).toHaveBeenCalledWith(filePath);
+      jest.spyOn(promises, 'readFile').mockImplementation(() => Promise.resolve(fileContents));
+        const result = await FsManager.readFile(filePath);
+    expect(promises.readFile).toHaveBeenCalledWith(filePath);
     expect(result.toString()).toEqual(fileContents);
+  });
+  // create a test fort base64Encode to have Buffer as input and return a string
+  it('should encode a file to base64', async () => {
+    const filePath = path.join(__dirname, 'file.txt');
+    const fileContents = 'Hello World';
+    const expectedBase64 = 'SGVsbG8gV29ybGQ='
+    jest.spyOn(promises, 'readFile').mockImplementation(() => Promise.resolve(fileContents));
+    const result = await FsManager.base64Encode(filePath);
+    expect(result).toEqual(expectedBase64);
+  });
+
+  it('should find the base name of a file', () => {
+    const expectedFileName = 'test.txt';
+    const result = FsManager.fileBaseName(expectedFileName);
+    expect(result).toEqual('test');
+  });
+
+  it('should find the type of a file', () => {
+    const expectedFileName = 'test.txt';
+    const result = FsManager.getType(expectedFileName);
+    expect(result).toEqual('txt');
   });
 });
