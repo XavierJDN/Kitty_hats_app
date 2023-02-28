af58e5a8c0350de50ffd282e01e10b2ea0ff42f7
Xavier Jouslin <jouslin@nowray.net>
2023-02-28 13:24:00 -0500

    feat: add form for search filter component

--- client/src/app/search-bar/search-bar.component.ts
+++ client/src/app/search-bar/search-bar.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent {
+  @Output() author: string = "";
+  @Output() owner: string  = "";
+  authors: string[] = []
+  owners: string[] = []
 
-  constructor() { }
+  form: FormGroup = new FormGroup({
+    author: new FormControl("", Validators.required),
+    owner: new FormControl("", Validators.required),
+  })
 
-  ngOnInit(): void {
+  isFormChanged() {
+    return this.owner === this.form.get('author')?.value || this.owner === this.form.get('owner')?.value;
   }
 
+  updateView(){
+    if(this.isFormChanged()){
+      return;
+    } 
+    this.author = this.form.get('author')?.value;
+    this.owner = this.form.get('owner')?.value;
+  }
 }
