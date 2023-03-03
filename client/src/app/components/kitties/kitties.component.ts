adf98bcc65aba51862bf441442e579ef53decdc3
Xavier Jouslin <jouslin@nowray.net>
2023-03-03 18:11:49 -0500

    feat: render kitties

--- client/src/app/components/kitties/kitties.component.ts
+++ client/src/app/components/kitties/kitties.component.ts
@@ -2,6 +2,7 @@ import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Kitty } from '@app/interface/kitty';
 import { CommunicationService } from '@app/services/communication/communication.service';
+import { KittiesManagerService } from '@app/services/kitties-manager/kitties-manager.service';
 
 @Component({
   selector: 'app-kitties',
@@ -10,24 +11,24 @@ import { CommunicationService } from '@app/services/communication/communication.
 })
 export class KittiesComponent {
   kitties: Kitty[] = [];
-  constructor(private communication: CommunicationService) {}
+
+  constructor(private communication: CommunicationService, public kittiesManager: KittiesManagerService) {}
+
   ngOnInit(): void {
     this.kitties = [];
-    this.communication
-      .getHatsKitties()
-      .subscribe((response: HttpResponse<string[]>) => {
-        response.body!.forEach((kitty: string) => {
-          this.communication
-            .getKitty(kitty)
-            .subscribe((response: HttpResponse<any>) => {
-              this.kitties.push({
-                name: response.body.name,
-                address: kitty,
-                img: response.body.image_url,
-                owner: response.body.owner.address,
-              });
-            });
-        });
-      });
+    this.kittiesManager.$kitty.subscribe((kitty: Kitty) => {
+      this.kitties.push(kitty);
+    });
+    this.kittiesManager.getAllHatsKitties();
+  }
+
+  nextPage(){
+    this.kitties = [];
+    this.kittiesManager.next();
+  }
+
+  previousPage(){
+    this.kitties = [];
+    this.kittiesManager.previous();
   }
 }
--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TokensComponent } from '@app/components/tokens/tokens.component';
+import { KittiesComponent } from '@app/components/kitties/kitties.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: TokensComponent},
-  { path: '**', redirectTo: '/home'}
+  { path: 'kitties', component: KittiesComponent },
+  { path: '**', redirectTo: '/home'},
 ];
 
 @NgModule({
